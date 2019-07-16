69c54c1d7c8a6d1e41ea4b19e961a4b9cfd9e261
Stephan D <duenkel.stephan@gmail.com>
2019-07-16 21:01:35 +0200

    new testcase - checking onSubmitButtonClicked

--- src/components/login/Login.test.js
+++ src/components/login/Login.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Login from "./Login";
+import App from "../../App";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { shallow, mount, render } from "enzyme";
@@ -11,22 +12,36 @@ import CardContent from "@material-ui/core/CardContent";
 
 configure({ adapter: new Adapter() });
 
-let wrapper;
+let wrapperApp;
+let wrapperLogin;
 
 beforeEach(() => {
-  wrapper = shallow(<Login />);
+  wrapperApp = shallow(<App />);
+  wrapperLogin = shallow(<Login setUsername={event => {wrapperApp.instance().setUsername(event)}} />);
 });
 
 describe("<Login />", () => {
-
-  it("renders an login area", () => {
-    expect(wrapper.find("h1")).toHaveLength(1);
-    expect(wrapper.find("form")).toHaveLength(1);
-    expect(wrapper.find(Card)).toHaveLength(1);
-    expect(wrapper.find(CardContent)).toHaveLength(1);
-    expect(wrapper.find(CardActions)).toHaveLength(1);
-    expect(wrapper.find(TextField)).toHaveLength(1);
-    expect(wrapper.find(Button)).toHaveLength(1);
+  it("renders the login area", () => {
+    expect(wrapperLogin.find("h1")).toHaveLength(1);
+    expect(wrapperLogin.find("form")).toHaveLength(1);
+    expect(wrapperLogin.find(Card)).toHaveLength(1);
+    expect(wrapperLogin.find(CardContent)).toHaveLength(1);
+    expect(wrapperLogin.find(CardActions)).toHaveLength(1);
+    expect(wrapperLogin.find(TextField)).toHaveLength(1);
+    expect(wrapperLogin.find(Button)).toHaveLength(1);
   });
 
+  it("checks the login username TextField", () => {
+    let textField = wrapperLogin.find(TextField);
+    textField.value = "test";
+    expect(textField.value).toEqual("test");
+  });
+
+  it("checks the onSubmitClick event", () => {
+    let button = wrapperLogin.find(Button);
+    wrapperLogin.setState({ username: "test" });
+    expect(wrapperLogin.state().username).toEqual("test");
+    button.simulate("click");
+    expect(wrapperApp.state().username).toEqual("test");
+  });
 });
